0ed997f5912bcee3c09e8c36390ca56173242148
Daniel Tressel <dhtressel@gmail.com>
2022-11-11 12:26:54 -0500

    created user model

--- models.py
+++ models.py
@@ -5,3 +5,27 @@ db = SQLAlchemy()
 def connect_db(app):
   db.app = app 
   db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(
+        db.String,
+        primary_key = True
+    )
+    password = db.Column(
+        db.String,
+        nullable = False
+    )
+    email = db.Column(
+        db.String,
+        nullable = False
+    )
+    first_name = db.Column(
+        db.String,
+        nullable = False
+    )    
+    last_name = db.Column(
+        db.String,
+        nullable = False
+    )
